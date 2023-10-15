3a99e6a4856b1da38557359db552e20f2a3e6645
RedFlame2112 <sagnik3@illinois.edu>
2023-10-14 22:32:48 -0500

    chore(page): Added initialization of basic pagemem
    We initialized part of the entries for the page directories
    
    We now need to set the page table

--- student-distrib/page.c
+++ student-distrib/page.c
@@ -1,7 +1,33 @@
+#include "page.h"
+
+
+void init_dir_entry(page_dir_entry_t *entry, uint32_t user, uint32_t present, uint32_t offset) {
+    entry->user = user;
+    entry->present = present;
+    entry->offset = offset;
+    entry->rw = 1;
+    entry->write_through = 0;
+    entry->cache_disable = 0;
+    entry->accessed = 0;
+    entry->size = 1;
+    entry->reserved = 0;
+}
+
+//initialize the first 4MB of memory with 4KB tables
 
 
 void page_init() {
     for (i = 0; i < MAX_SPACES; i++) {
-        
+        if(i == KERNEL_IDX){
+          //set up kernel memory
+          init_dir_entry(&page_directory[i], 0, 1, ((uint32_t)KERNEL_ADDR / FOUR_KB));
+        }
+        else if (i == USER_IDX) {
+          //set up user virtual memory
+          init_dir_entry(&page_directory[i], 1, 1, ((uint32_t)USER_ADDR / FOUR_KB));
+        } else {
+          //set up remaining memory
+          init_dir_entry(&page_directory[i], 0, 0, 0);
+        }
     }
-}
+} 
--- student-distrib/page.h
+++ student-distrib/page.h
@@ -3,12 +3,30 @@
 
 //Define the page size
 #define FOUR_KB 4096 //2^12 bytes
-
+#define KERNEL_ADDR 0x400000 //start of kmem in physical media
+#define USER_ADDR 0x800000 //start of user mem in physical media
 #define MAX_SPACES 1024 //number of pages in dir
+#define KERNEL_IDX 1
+#define USER_IDX 32
+
 
 void page_init();
+void init_dir_entry(page_dir_entry_t *entry, uint32_t user, uint32_t present, uint32_t offset);
 
+typedef struct __attribute__((packed)) page_dir_entry_t {
+  uint32_t present: 1;
+  uint32_t rw: 1;
+  uint32_t user: 1;
+  uint32_t write_through: 1;
+  uint32_t cache_disable: 1;
+  uint32_t accessed: 1;
+  uint32_t ignored: 1;
+  uint32_t reserved: 1;
+  uint32_t size: 1;
+  uint32_t avail: 3; //11:9
+  uint32_t offset: 20; //31:12
+} page_dir_entry_t;
 
-uint32_t page_directory[MAX_SPACES] __attribute__((aligned(4096)));
+page_dir_entry_t page_directory[MAX_SPACES] __attribute__((aligned(4096)));
 
 #endif

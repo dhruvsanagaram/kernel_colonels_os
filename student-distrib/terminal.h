752df3e601928e0ceddab58eaa371987ac30b6eb
Thammakhoune <rct4@illinois.edu>
2023-10-21 15:24:04 -0500

    Minor fixes

--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -4,14 +4,14 @@
 #define MAX_BUF_SIZE 128
 
 
-extern static char key_buf[MAX_BUF_SIZE];
-extern volatile static int numChars = 0;
-extern volatile static int enterKeyPressed = 0;
+extern char key_buf[MAX_BUF_SIZE];
+int numChars = 0;
+int enterKeyPressed = 0;
 
 
 //term driver syscalls
-int32_t terminal_read(int32_t fd, char *buf, int32_t nbytes);
-int32_t terminal_write(int32_t fd, const char *buf, int32_t nbytes);
+int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t terminal_write(int32_t fd, const void *buf, int32_t nbytes);
 int32_t terminal_open(const uint8_t *filename);
 int32_t terminal_close(int32_t fd);
 
